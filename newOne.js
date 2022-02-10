66f93bf2118e4277103772067307beb2e4304837
Urdo <karol.ugarenko@gmail.com>
2022-02-10 21:18:57 +0100

    added focus and blur

--- newOne.js
+++ newOne.js
@@ -5,13 +5,25 @@ const badMessage = "wronge password";
 
 
 const showMessage = (e) => {
+    document.querySelector("div").textContent = badMessage;
     passwords.forEach((item, index) => {
         if (item.toLowerCase() === e.target.value.toLowerCase()){ //dont check Lower Upper Case
             //console.log("work");
             document.querySelector("div").textContent = messages[index]; // show message
+            
         }
     })
 }
 
 
 input.addEventListener("input", showMessage);
+
+//FOCUS
+input.addEventListener('focus', (e) => { //change backgroud color when write password
+    e.target.classList.add('active');
+    });
+   
+   //BLUR
+   input.addEventListener('blur', (e) => { 
+   e.target.classList.remove('active');
+   });
