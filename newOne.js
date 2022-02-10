08461216e1d9fb6ba865d82047274e0025d314fe
Urdo <karol.ugarenko@gmail.com>
2022-02-10 21:04:13 +0100

    problem with forEach []

--- newOne.js
+++ newOne.js
@@ -1,11 +1,17 @@
 const input = document.querySelector("input");
 const passwords = ["admin", "user"];
 const messages = ["correct", "hello"];
+const badMessage = "wrong password";
 
-const showMessage = (e) => {
-    const text = e.target.value; 
-    console.log(text); 
+input.addEventListener("input", (e) =>{
 
-};
+    const text = e.target.value;
+    document.querySelector('div').textContent=badMessage;
+    
+passwords.forEach(password, index) => {
+    if (password === text) {
+       console.log(password, index);
+    }}})
 
-input.addEventListener("input", showMessage);
+
+
