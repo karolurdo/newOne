fba89aa3c2a227f23a324cdb7111b3cc8d8c7127
Urdo <karol.ugarenko@gmail.com>
2022-02-10 21:16:05 +0100

    show message if passowod correct

--- newOne.js
+++ newOne.js
@@ -3,9 +3,10 @@ const passwords = ["admin", "user"];
 const messages = ["correct", "hello"];
 
 const showMessage = (e) => {
-    passwords.forEach((item) => {
+    passwords.forEach((item, index) => {
         if (item.toLowerCase() === e.target.value.toLowerCase()){ //dont check Lower Upper Case
-            console.log("work");
+            //console.log("work");
+            document.querySelector("div").textContent = messages[index]; // show message
         }
     })
 }
